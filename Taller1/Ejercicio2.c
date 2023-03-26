bbab5bf6087974afb003b4e4cd4d1fcf8fd21e65
VictoriaAlvarado <124613909+VictoriaAlvarado@users.noreply.github.com>
2023-03-26 01:09:06 -0400

    Update Ejercicio2.c

--- Taller1/Ejercicio2.c
+++ Taller1/Ejercicio2.c
@@ -1,27 +1,39 @@
 #include <stdio.h>
 
-int main(){
-   int n, num, i, a, flip=0, cont=0;
+int main() {
+    int num,num_inv,dig,cont3,cont5,aux;
+    int read;
+    char answ;
     
-    printf("ingrese cuantos numeros desea ingresar/n");
-    do{
-        scanf("%d",&n);
-    }while(n<0);
-    
-    for (i=0; i<n;i++){
-        printf("Ingrese un numero");
-        scanf("d%",&num);
-             while (num !=0){
-                 a= num%10;
-                 num= num/10;
-                 flip= (flip *10)+a;
-                     if (num==3 || num ==5){
-                     cont++;
-                     }
-                 
-             }
-    }
-    printf("\nE1 numero de veces que ingreso el 3 y el 5: %d\n", cont);
-    
-return 0;
+    read=1;
+    cont3=0;
+    cont5=0;
+    printf ("Ejercicio 2:\n");
+    do {
+        num_inv=0;
+        printf ("\nIngrese un número: \n");
+        scanf (" %i",&num);
+        aux=num;
+        while (num>0) {
+            dig=num%10;
+            num_inv=(num_inv*10)+dig;
+            num=num/10;
+            if (dig==3) {
+                cont3=cont3+1;
+            } else if (dig==5) {
+                cont5=cont5+1;
+            }
+        }
+
+        if (aux>99) {
+            printf ("\nNúmero invertido:%i\n",num_inv);
+        }
+        printf ("\n¿Leer otro número?(y-n)\n");
+        scanf (" %c",&answ);
+        if (answ!='y') {
+            read=0;
+        }
+    } while (read==1);
+    printf ("\nVeces que se ingresó el 5: %i\nVeces que se ingresó el 3: %i\n",cont5,cont3);
+    return 0;
 }
